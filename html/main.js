de6de2c6f96a0d67724f030592b0ff1ddd250076
Yuzunoha <studysteadystandby@gmail.com>
2021-03-04 19:05:49 +0900

    async-await

--- html/main.js
+++ html/main.js
@@ -1,19 +1,25 @@
 'use strict';
 
-const p = console.log;
-
+const p1 = document.getElementById('p1');
 const urlRandomPrefecture = 'http://localhost:20180/random-prefecture';
 const urlPrefecturalCapital = 'http://localhost:20080/prefectural-capital/';
 
-fetch(urlRandomPrefecture)
-  .then((res) => res.json())
-  .then((jsonRP) => {
-    const prefecture = jsonRP.prefecture;
-    fetch(urlPrefecturalCapital + encodeURI(prefecture))
-      .then((res) => res.json())
-      .then((jsonPC) => {
-        const capital = jsonPC[0]._value;
-        const msg = `${prefecture}の首都は${capital}です。`;
-        p(msg);
-      });
-  });
+const jsonReturn = (obj) => obj.json();
+
+const asyncMain = async () => {
+  const prefecture = await fetch(urlRandomPrefecture)
+    .then(jsonReturn)
+    .then((json) => json.prefecture);
+
+  const urlPrefecturalCapitalWithParam = urlPrefecturalCapital + encodeURI(prefecture);
+
+  const capital = await fetch(urlPrefecturalCapitalWithParam)
+    .then(jsonReturn)
+    .then((json) => json[0]._value);
+
+  const msg = `${prefecture}の首都は${capital}です。`;
+
+  p1.innerHTML = msg;
+};
+
+asyncMain();
