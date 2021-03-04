99f119900aceba17ec3acd6ea7b27e8388273771
Yuzunoha <studysteadystandby@gmail.com>
2021-03-04 18:02:33 +0900

    できた

--- html/main.js
+++ html/main.js
@@ -5,5 +5,8 @@ const p = console.log;
 const urlRandomPrefecture = 'http://localhost:20180/random-prefecture';
 const url = 'http://localhost:20080/prefectural-capital/' + encodeURI('東京都');
 
-p(unescape(encodeURI('東京都')));
-p(encodeURI('東京都'));
+p(url);
+
+fetch(url)
+  .then((res) => res.json())
+  .then(p);
