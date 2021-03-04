37c028ba1f4fa51ff87240268c8a52eae7cc5eb9
Yuzunoha <studysteadystandby@gmail.com>
2021-03-04 19:09:48 +0900

    returnJson

--- html/main.js
+++ html/main.js
@@ -3,15 +3,15 @@
 const p1 = document.getElementById('p1');
 const urlRandomPrefecture = 'http://localhost:20180/random-prefecture';
 const urlPrefecturalCapital = 'http://localhost:20080/prefectural-capital/';
-const jsonReturn = (obj) => obj.json();
+const returnJson = (obj) => obj.json();
 
 const asyncMain = async () => {
   const prefecture = await fetch(urlRandomPrefecture)
-    .then(jsonReturn)
+    .then(returnJson)
     .then((json) => json.prefecture);
 
   const capital = await fetch(urlPrefecturalCapital + encodeURI(prefecture))
-    .then(jsonReturn)
+    .then(returnJson)
     .then((json) => json[0]._value);
 
   const msg = `${prefecture}の首都は${capital}です。`;
