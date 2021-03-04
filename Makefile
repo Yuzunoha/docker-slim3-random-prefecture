3b97ef57bbe2b943c01087ca3c3dfc30d0b040d5
Yuzunoha <studysteadystandby@gmail.com>
2021-03-04 15:10:18 +0900

    コピペした

---
+++ Makefile
@@ -0,0 +1,3 @@
+run:
+	cd server_prefectural_capital; pwd;
+	cd server_random_prefecture; pwd;
---
+++ server_random_prefecture/html/public/index.php
@@ -0,0 +1,36 @@
+<?php
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+function pdo()
+{
+  $dsn = 'mysql:dbname=docker_db;host=mysql';
+  $user = 'docker_db_user';
+  $password = 'docker_db_user_pass';
+  $pdo = new PDO($dsn, $user, $password);
+  return $pdo;
+}
+
+$app = new \Slim\App;
+
+$app->get('/prefectural-capital', function (Request $request, Response $response) {
+  $sql = 'select * from kvs1';
+  $sth = pdo()->prepare($sql);
+  $sth->execute();
+  $data = $sth->fetchAll(PDO::FETCH_ASSOC);
+  return $response->withJson($data, 200, JSON_UNESCAPED_UNICODE);
+});
+
+$app->get('/prefectural-capital/{prefecture}', function (Request $request, Response $response) {
+  $sql = 'select * from kvs1 where _key = :_key';
+  $param = [':_key' => $request->getAttribute('prefecture')];
+  $sth = pdo()->prepare($sql);
+  $sth->execute($param);
+  $data = $sth->fetchAll(PDO::FETCH_ASSOC);
+  return $response->withJson($data, 200, JSON_UNESCAPED_UNICODE);
+});
+
+$app->run();
